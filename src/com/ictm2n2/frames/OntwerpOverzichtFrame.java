ae12bc6b22186d9e72b073390779b2302437f7b5
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-04-21 10:08:27 +0200

    OverzichtFrame code

--- src/com/ictm2n2/frames/OntwerpOverzichtFrame.java
+++ src/com/ictm2n2/frames/OntwerpOverzichtFrame.java
@@ -1,9 +1,12 @@
 package com.ictm2n2.frames;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -18,29 +21,37 @@ public class OntwerpOverzichtFrame extends JFrame {
     private JButton verwijderButton;
     private JButton nieuwOntwerpButton;
     private JPanel container;
+    private JPanel overzichtPanel;
 
     public OntwerpOverzichtFrame() {
         setSize(1600, 900);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setTitle("Ontwerpen Overzicht");
-        setLayout(new GridLayout(2, 1));
 
         aanGemaakteOntwerpenDropdown = new JComboBox<String>(new String[] { "Test", "test" });
         ontwerpenOverzichtTitle = new JLabel("<html><h1>Ontwerp Overzicht</h1></html>", JLabel.CENTER);
         bewerkButton = new JButton("Bewerken");
         verwijderButton = new JButton("Verwijderen");
         nieuwOntwerpButton = new JButton("Nieuw ontwerp");
+        container = new JPanel();
+        overzichtPanel = new JPanel();
 
-        container = new JPanel(new FlowLayout());
+        aanGemaakteOntwerpenDropdown.setMaximumSize(new Dimension(100, 30));
+
+        container.setLayout(new BorderLayout());
         container.setPreferredSize(new Dimension(900, 600));
         container.setBorder(new EmptyBorder(20, 20, 20, 20));
 
-        container.add(aanGemaakteOntwerpenDropdown);
-        container.add(bewerkButton);
-        container.add(verwijderButton);
-        container.add(nieuwOntwerpButton);
+        overzichtPanel.setLayout(new BoxLayout(overzichtPanel, BoxLayout.X_AXIS));
 
-        add(ontwerpenOverzichtTitle);
+        container.add(ontwerpenOverzichtTitle, BorderLayout.NORTH);
+        container.add(overzichtPanel, BorderLayout.CENTER);
+
+        overzichtPanel.add(aanGemaakteOntwerpenDropdown);
+        overzichtPanel.add(bewerkButton);
+        overzichtPanel.add(verwijderButton);
+        overzichtPanel.add(nieuwOntwerpButton);
+
         add(container);
 
         setVisible(true);
