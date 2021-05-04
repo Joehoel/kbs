f3654f05ea47fcaacf1e41aa0253b70e3349eb04
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-05-04 12:11:32 +0200

    Monitoren start

--- src/com/ictm2n2/frames/MonitorPanel.java
+++ src/com/ictm2n2/frames/MonitorPanel.java
@@ -1,12 +1,30 @@
 package com.ictm2n2.frames;
 
-import com.ictm2n2.resources.*;
+import java.sql.ResultSet;
 
-import javax.swing.*;
+import javax.swing.JPanel;
+
+import com.ictm2n2.resources.database.Database;
+import com.ictm2n2.resources.database.Query;
 
 public class MonitorPanel extends JPanel {
 
     public MonitorPanel() {
+        setLayout(null);
+
+        try {
+            Database db = new Database("nerdygadgets", "monitoring", "Iloveberrit3!$");
+
+            Query q = new Query();
+            q.select(null).from("status");
+            ResultSet rs = db.select(q);
+
+            while (rs.next()) {
+                String processorBelasting = rs.getString("processor_belasting");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
