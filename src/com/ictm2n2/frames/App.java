590e606087ac8ff3d1bde8c88f1c8489fc97aa43
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-05-21 14:46:15 +0200

    Fix it

--- src/com/ictm2n2/frames/App.java
+++ src/com/ictm2n2/frames/App.java
@@ -27,6 +27,7 @@ public class App extends JFrame implements ActionListener {
         setLayout(null);
 
         try {
+            URL resource = getClass().getResource("../assets/KBS.png");
             BufferedImage image = ImageIO.read(resource);
             setIconImage(image);
 
