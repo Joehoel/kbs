a888e5b7e5b03a6dfa64711a052d99698c217df7
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-05-27 09:11:20 +0200

    Remove double code

--- src/com/ictm2n2/frames/App.java
+++ src/com/ictm2n2/frames/App.java
@@ -20,18 +20,7 @@ public class App extends JFrame implements ActionListener {
     private InstellingenPanel instellingenPanel = new InstellingenPanel();
 
     public App() {
-
-        setSize(900, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("NerdyGadgets | ICTm2n2");
-        setResizable(false);
-        setLayout(null);
-
         try {
-//            URL resource = getClass().getResource("../assets/KBS.png");
-//            BufferedImage image = ImageIO.read(resource);
-//            setIconImage(image);
-
             setSize(900, 600);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setTitle("NerdyGadgets | ICTm2n2");
